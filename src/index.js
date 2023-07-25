4f5c67073cc4fca73ae212008d99cb88c9527ae5
mariante0dorescu <broinkus@gmail.com>
2023-07-25 16:49:58 +0300

    small setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import "./styles.css";
 import "./styles.scss";
-console.log("hello world!");
+import "./api_key.js"
+
+
+
+
+// FUNCTIONS
+// fetch data
+// search city
+// create html content
+// background changer depending on the weather
+
+// COMPONENTS
+// search form
+// show data
+// graph???
