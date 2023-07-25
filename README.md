3e24cfd3b49df34e8c1b56fd543dd8126c3d41ff
mariante0dorescu <broinkus@gmail.com>
2023-07-25 16:09:15 +0300

    webpack setup finished

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # WeatherApp
 Weather app created for the Odin Project learning journey.
+
+##Live
+You can check it live here:
+
+### Building and running on localhost
+
+First install dependencies:
+
+```sh
+npm install
+```
+
+To create a production build:
+
+```sh
+npm run build-prod
+```
+
+To create a development build:
+
+```sh
+npm run build-dev
+```
+
+## Running
+
+```sh
+node dist/bundle.js
+```
+
+## Testing
+
+To run unit tests:
+
+```sh
+npm test
+```
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    assetModuleFilename: 'images/[hash][ext][query]',
+    clean: true,
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist')
+    },
+    open: true, 
+    hot: true, 
+    compress: true,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        type: "asset/resource"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "WeatherApp",
+      templateContent: ({ htmlWebpackPlugin }) => '<!DOCTYPE html><html><head><meta charset=\"utf-8\"><meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="shortcut icon" href="#" type="image/x-icon"><title>' + htmlWebpackPlugin.options.title + '</title></head><body><div id=\"app\"></div></body></html>',
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin()
+  ]
+};
+
+module.exports = config;
